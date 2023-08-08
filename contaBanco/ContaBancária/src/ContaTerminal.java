bc67b432eafdc1b675cd7731516ebb58e73f10a2
Gabriel Chaves <gabriel11schaves@gmail.com>
2023-08-08 20:57:13 -0300

    contaTerminal

---
+++ contaBanco/ContaBancária/src/ContaTerminal.java
@@ -0,0 +1,29 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+  
+public static void main(String[] args) {
+    try (Scanner scanner = new Scanner(System.in).useLocale(Locale.US)) {
+        double saldo= 237.48;
+
+        System.out.println("Digite o seu nome");
+
+            String nomeCliente = scanner.next();
+
+        System.out.println("Digite o número da conta");
+
+            int numeroConta =scanner.nextInt();
+
+        System.out.println("Digite o número da agência");
+
+            String numeroAgencia= scanner.next();
+   
+        System.out.printf(" Olá " + (nomeCliente) + " ,obrigado por criar uma conta em nosso banco, sua agência é " + (numeroAgencia) + ", conta " + (numeroConta) + " e seu saldo "+(saldo)+" já está disponível para saque " );
+    }
+        
+    
+
+    }
+}
