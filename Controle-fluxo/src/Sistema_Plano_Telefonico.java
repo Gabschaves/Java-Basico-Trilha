5f5bd22d71e3eba73f2a546aee83954cea82bcff
Gabriel Chaves <gabriel11schaves@gmail.com>
2023-08-09 15:19:23 -0300

    controleFluxo

---
+++ Controle-fluxo/src/Sistema_Plano_Telefonico.java
@@ -0,0 +1,19 @@
+public class Sistema_Plano_Telefonico {
+    public static void main(String[] args) {
+        String plano="M";
+        switch (plano){
+            case "T":{
+                System.out.println("5GB de Youtube");
+            }
+            case "M":{
+                System.out.println("Whats e instagram gratuitos");
+            }
+            case "B":{
+                System.out.println("100 minutos de ligação");
+            }
+
+        }
+
+    }
+
+}
